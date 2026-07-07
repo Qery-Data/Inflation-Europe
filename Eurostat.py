adcb9e3d7a354095ded03c32bcaa784738ad4b1e
alf.lonne <alf.lonne@qery.no>
2026-07-07 21:02:02 +0200

    Switch Eurostat.py to prc_hicp_minr (prc_hicp_manr is discontinued)
    Eurostat discontinued prc_hicp_manr, replacing it with prc_hicp_minr
    (HICP - ECOICOP ver.2 - indices and rates of change). The old dataset
    stopped receiving updates after 2026-02-06 (frozen at December 2025),
    so every chart fed by this script has been silently stuck 6+ months
    stale even though the fetch job kept running successfully.
    
    Changes needed for the new dataset:
    - coicop= -> coicop18= (renamed dimension)
    - CP00 -> TOTAL (renamed code for the all-items aggregate; the special
      aggregates FOOD/NRG/SERV/IGD_NNRG are unchanged)
    - added explicit unit=RCH_A (the new dataset bundles index levels and
      multiple rate-of-change variants together, so it must be selected)
    - pivot() index label for the components pulls changed from
      "Classification of individual consumption by purpose (COICOP)" to
      "...(COICOP) - 2018" to match the renamed dimension label
    
    Verified locally: all 8 pulls succeed, data now extends to 2026-06
    (vs. 2025-12 before), Euro area/EU27 labels still rename cleanly.

--- Eurostat.py
+++ Eurostat.py
@@ -9,56 +9,56 @@ rename_dict = {
 }
 
 #Euro Area Flash Estimate
-dataset = pyjstat.Dataset.read('https://ec.europa.eu/eurostat/api/dissemination/statistics/1.0/data/prc_hicp_manr?lang=en&lastTimePeriod=61&coicop=CP00&geo=EA&geo=BE&geo=DE&geo=EE&geo=IE&geo=EL&geo=ES&geo=FR&geo=HR&geo=IT&geo=CY&geo=LV&geo=LT&geo=LU&geo=MT&geo=NL&geo=AT&geo=PT&geo=SI&geo=SK&geo=FI')
+dataset = pyjstat.Dataset.read('https://ec.europa.eu/eurostat/api/dissemination/statistics/1.0/data/prc_hicp_minr?lang=en&lastTimePeriod=61&unit=RCH_A&coicop18=TOTAL&geo=EA&geo=BE&geo=DE&geo=EE&geo=IE&geo=EL&geo=ES&geo=FR&geo=HR&geo=IT&geo=CY&geo=LV&geo=LT&geo=LU&geo=MT&geo=NL&geo=AT&geo=PT&geo=SI&geo=SK&geo=FI')
 df = dataset.write('dataframe')
 df.replace(rename_dict, regex=True, inplace=True)
 df_new = df.pivot(index='Geopolitical entity (reporting)', columns='Time', values='value')
 df_new.to_csv('data/Eurostat_Inflation_EA_HICP_Time.csv', index=True)
 
 #Europe Latest Data
-dataset = pyjstat.Dataset.read('https://ec.europa.eu/eurostat/api/dissemination/statistics/1.0/data/prc_hicp_manr?lang=en&lastTimePeriod=61&coicop=CP00&geo=EU&geo=EA&geo=BE&geo=BG&geo=CZ&geo=DK&geo=DE&geo=EE&geo=IE&geo=EL&geo=ES&geo=FR&geo=HR&geo=IT&geo=CY&geo=LV&geo=LT&geo=LU&geo=HU&geo=MT&geo=NL&geo=AT&geo=PL&geo=PT&geo=RO&geo=SI&geo=SK&geo=FI&geo=SE&geo=NO&geo=IS&geo=CH')
+dataset = pyjstat.Dataset.read('https://ec.europa.eu/eurostat/api/dissemination/statistics/1.0/data/prc_hicp_minr?lang=en&lastTimePeriod=61&unit=RCH_A&coicop18=TOTAL&geo=EU&geo=EA&geo=BE&geo=BG&geo=CZ&geo=DK&geo=DE&geo=EE&geo=IE&geo=EL&geo=ES&geo=FR&geo=HR&geo=IT&geo=CY&geo=LV&geo=LT&geo=LU&geo=HU&geo=MT&geo=NL&geo=AT&geo=PL&geo=PT&geo=RO&geo=SI&geo=SK&geo=FI&geo=SE&geo=NO&geo=IS&geo=CH')
 df = dataset.write('dataframe')
 df.replace(rename_dict, regex=True, inplace=True)
 df_new = df.pivot(index='Geopolitical entity (reporting)', columns='Time', values='value')
 df_new.to_csv('data/Eurostat_Inflation_All_HICP_Time.csv', index=True)
 
 #EU27 Main Components
-dataset = pyjstat.Dataset.read('https://ec.europa.eu/eurostat/api/dissemination/statistics/1.0/data/prc_hicp_manr?lang=en&lastTimePeriod=61&coicop=CP00&geo=EU&coicop=FOOD&coicop=IGD_NNRG&coicop=NRG&coicop=SERV')
+dataset = pyjstat.Dataset.read('https://ec.europa.eu/eurostat/api/dissemination/statistics/1.0/data/prc_hicp_minr?lang=en&lastTimePeriod=61&unit=RCH_A&coicop18=TOTAL&geo=EU&coicop18=FOOD&coicop18=IGD_NNRG&coicop18=NRG&coicop18=SERV')
 df = dataset.write('dataframe')
 df.replace(rename_dict, regex=True, inplace=True)
-df_new = df.pivot(index='Classification of individual consumption by purpose (COICOP)', columns='Time', values='value')
+df_new = df.pivot(index='Classification of individual consumption by purpose (COICOP) - 2018', columns='Time', values='value')
 df_new.to_csv('data/Eurostat_Inflation_EU27_Components_Time.csv', index=True)
 
 #EA Main Components
-dataset = pyjstat.Dataset.read('https://ec.europa.eu/eurostat/api/dissemination/statistics/1.0/data/prc_hicp_manr?lang=en&lastTimePeriod=61&coicop=CP00&geo=EA&coicop=FOOD&coicop=IGD_NNRG&coicop=NRG&coicop=SERV')
+dataset = pyjstat.Dataset.read('https://ec.europa.eu/eurostat/api/dissemination/statistics/1.0/data/prc_hicp_minr?lang=en&lastTimePeriod=61&unit=RCH_A&coicop18=TOTAL&geo=EA&coicop18=FOOD&coicop18=IGD_NNRG&coicop18=NRG&coicop18=SERV')
 df = dataset.write('dataframe')
 df.replace(rename_dict, regex=True, inplace=True)
-df_new = df.pivot(index='Classification of individual consumption by purpose (COICOP)', columns='Time', values='value')
+df_new = df.pivot(index='Classification of individual consumption by purpose (COICOP) - 2018', columns='Time', values='value')
 df_new.to_csv('data/Eurostat_Inflation_EA_Components_Time.csv', index=True)
 
-#Europe Food 
-dataset = pyjstat.Dataset.read('https://ec.europa.eu/eurostat/api/dissemination/statistics/1.0/data/prc_hicp_manr?lang=en&lastTimePeriod=61&coicop=FOOD&geo=EU&geo=EA&geo=BE&geo=BG&geo=CZ&geo=DK&geo=DE&geo=EE&geo=IE&geo=EL&geo=ES&geo=FR&geo=HR&geo=IT&geo=CY&geo=LV&geo=LT&geo=LU&geo=HU&geo=MT&geo=NL&geo=AT&geo=PL&geo=PT&geo=RO&geo=SI&geo=SK&geo=FI&geo=SE&geo=NO&geo=IS&geo=CH')
+#Europe Food
+dataset = pyjstat.Dataset.read('https://ec.europa.eu/eurostat/api/dissemination/statistics/1.0/data/prc_hicp_minr?lang=en&lastTimePeriod=61&unit=RCH_A&coicop18=FOOD&geo=EU&geo=EA&geo=BE&geo=BG&geo=CZ&geo=DK&geo=DE&geo=EE&geo=IE&geo=EL&geo=ES&geo=FR&geo=HR&geo=IT&geo=CY&geo=LV&geo=LT&geo=LU&geo=HU&geo=MT&geo=NL&geo=AT&geo=PL&geo=PT&geo=RO&geo=SI&geo=SK&geo=FI&geo=SE&geo=NO&geo=IS&geo=CH')
 df = dataset.write('dataframe')
 df.replace(rename_dict, regex=True, inplace=True)
 df_new = df.pivot(index='Geopolitical entity (reporting)', columns='Time', values='value')
 df_new.to_csv('data/Eurostat_Inflation_All_Food_Time.csv', index=True)
 
-#Europe Services 
-dataset = pyjstat.Dataset.read('https://ec.europa.eu/eurostat/api/dissemination/statistics/1.0/data/prc_hicp_manr?lang=en&lastTimePeriod=61&coicop=SERV&geo=EU&geo=EA&geo=BE&geo=BG&geo=CZ&geo=DK&geo=DE&geo=EE&geo=IE&geo=EL&geo=ES&geo=FR&geo=HR&geo=IT&geo=CY&geo=LV&geo=LT&geo=LU&geo=HU&geo=MT&geo=NL&geo=AT&geo=PL&geo=PT&geo=RO&geo=SI&geo=SK&geo=FI&geo=SE&geo=NO&geo=IS&geo=CH')
+#Europe Services
+dataset = pyjstat.Dataset.read('https://ec.europa.eu/eurostat/api/dissemination/statistics/1.0/data/prc_hicp_minr?lang=en&lastTimePeriod=61&unit=RCH_A&coicop18=SERV&geo=EU&geo=EA&geo=BE&geo=BG&geo=CZ&geo=DK&geo=DE&geo=EE&geo=IE&geo=EL&geo=ES&geo=FR&geo=HR&geo=IT&geo=CY&geo=LV&geo=LT&geo=LU&geo=HU&geo=MT&geo=NL&geo=AT&geo=PL&geo=PT&geo=RO&geo=SI&geo=SK&geo=FI&geo=SE&geo=NO&geo=IS&geo=CH')
 df = dataset.write('dataframe')
 df.replace(rename_dict, regex=True, inplace=True)
 df_new = df.pivot(index='Geopolitical entity (reporting)', columns='Time', values='value')
 df_new.to_csv('data/Eurostat_Inflation_All_Services_Time.csv', index=True)
 
-#Europe Energy 
-dataset = pyjstat.Dataset.read('https://ec.europa.eu/eurostat/api/dissemination/statistics/1.0/data/prc_hicp_manr?lang=en&lastTimePeriod=61&coicop=NRG&geo=EU&geo=EA&geo=BE&geo=BG&geo=CZ&geo=DK&geo=DE&geo=EE&geo=IE&geo=EL&geo=ES&geo=FR&geo=HR&geo=IT&geo=CY&geo=LV&geo=LT&geo=LU&geo=HU&geo=MT&geo=NL&geo=AT&geo=PL&geo=PT&geo=RO&geo=SI&geo=SK&geo=FI&geo=SE&geo=NO&geo=IS&geo=CH')
+#Europe Energy
+dataset = pyjstat.Dataset.read('https://ec.europa.eu/eurostat/api/dissemination/statistics/1.0/data/prc_hicp_minr?lang=en&lastTimePeriod=61&unit=RCH_A&coicop18=NRG&geo=EU&geo=EA&geo=BE&geo=BG&geo=CZ&geo=DK&geo=DE&geo=EE&geo=IE&geo=EL&geo=ES&geo=FR&geo=HR&geo=IT&geo=CY&geo=LV&geo=LT&geo=LU&geo=HU&geo=MT&geo=NL&geo=AT&geo=PL&geo=PT&geo=RO&geo=SI&geo=SK&geo=FI&geo=SE&geo=NO&geo=IS&geo=CH')
 df = dataset.write('dataframe')
 df.replace(rename_dict, regex=True, inplace=True)
 df_new = df.pivot(index='Geopolitical entity (reporting)', columns='Time', values='value')
 df_new.to_csv('data/Eurostat_Inflation_All_Energy_Time.csv', index=True)
 
-#Europe Non-energy industrial goods 
-dataset = pyjstat.Dataset.read('https://ec.europa.eu/eurostat/api/dissemination/statistics/1.0/data/prc_hicp_manr?lang=en&lastTimePeriod=61&coicop=IGD_NNRG&geo=EU&geo=EA&geo=BE&geo=BG&geo=CZ&geo=DK&geo=DE&geo=EE&geo=IE&geo=EL&geo=ES&geo=FR&geo=HR&geo=IT&geo=CY&geo=LV&geo=LT&geo=LU&geo=HU&geo=MT&geo=NL&geo=AT&geo=PL&geo=PT&geo=RO&geo=SI&geo=SK&geo=FI&geo=SE&geo=NO&geo=IS&geo=CH')
+#Europe Non-energy industrial goods
+dataset = pyjstat.Dataset.read('https://ec.europa.eu/eurostat/api/dissemination/statistics/1.0/data/prc_hicp_minr?lang=en&lastTimePeriod=61&unit=RCH_A&coicop18=IGD_NNRG&geo=EU&geo=EA&geo=BE&geo=BG&geo=CZ&geo=DK&geo=DE&geo=EE&geo=IE&geo=EL&geo=ES&geo=FR&geo=HR&geo=IT&geo=CY&geo=LV&geo=LT&geo=LU&geo=HU&geo=MT&geo=NL&geo=AT&geo=PL&geo=PT&geo=RO&geo=SI&geo=SK&geo=FI&geo=SE&geo=NO&geo=IS&geo=CH')
 df = dataset.write('dataframe')
 df.replace(rename_dict, regex=True, inplace=True)
 df_new = df.pivot(index='Geopolitical entity (reporting)', columns='Time', values='value')
